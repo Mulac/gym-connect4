633bf7512fd9ed5a7267dfde9d6eaa07dcb8dfb7
Mulac <wallbridge.calum@gmail.com>
2020-04-24 20:30:35 +0100

    seperated and fixed connect4 game logic

---
+++ gym_connect4/envs/connect4.py
@@ -0,0 +1,74 @@
+import numpy as np
+from itertools import groupby
+import random
+
+class Connect4:
+  """
+  Simple connect4 game logic.  For two players 1 & 2.
+
+  Attributes
+    + board           {index: column}
+    + possible_moves  [column indexes]
+    + player          1 or 2
+    + winner          0 or 1 or 2 -> gameover
+                      None        -> in play
+
+  Methods
+    + show
+    + make_move( column index )
+  """
+  def __init__(self, cols=7, rows=6):
+    self.cols, self.rows = cols, rows
+    self.board = np.zeros(shape=(self.cols, self.rows), dtype=np.int8)
+    self.player = 1
+    self.winner = None
+
+  @property
+  def legal_moves(self):
+    return [i for i in range(self.cols) if self.board[i][self.rows - 1] == 0]
+      
+  def make_move(self, move):
+    assert move in self.legal_moves
+        
+    # Play their move in the top of the specified column
+    top_of_col = np.where(self.board[move] == 0)[0][0]
+    self.board[move][top_of_col] = self.player
+        
+    # Check if the game is over
+    self.winner = 1 if self.is_winner(1) \
+      else -1 if self.is_winner(-1) \
+        else 0 if self.board.all() \
+          else None
+
+    # Switch the player
+    self.player *= -1
+          
+  def is_winner(self, player):
+    brd = self.board
+    # Check horizontal
+    for c in range(self.cols - 3):
+      for r in range(self.rows):
+        if player == brd[c][r] == brd[c][r+1] == brd[c][r+2] == brd[c][r+3]:
+          return True
+
+    # Check vertical 
+    for c in range(self.cols):
+      for r in range(self.rows - 3):
+        if player == brd[c][r] == brd[c+1][r] == brd[c+2][r] == brd[c+3][r]:
+          return True
+
+    # Check positively sloped diaganols
+    for c in range(self.cols - 3):
+      for r in range(self.rows - 3):
+        if player == brd[c][r] == brd[c+1][r+1] == brd[c+2][r+2] == brd[c+3][r+3]:
+          return True
+
+    # Check negatively sloped diaganols
+    for c in range(self.cols - 3):
+      for r in range(3, self.rows):
+        if player == brd[c][r] == brd[c-1][r+1] == brd[c-2][r+2] == brd[c-3][r+3]:
+          return True
+
+  def show(self):
+    print()
+    print(np.rot90(self.board))
--- gym_connect4/envs/connect4_env.py
+++ gym_connect4/envs/connect4_env.py
@@ -2,9 +2,7 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-from itertools import groupby
-import numpy as np
-import random
+from gym_connect4.envs.connect4 import Connect4
 
 
 class Connect4Env(gym.Env):
@@ -43,88 +41,3 @@ class Connect4Env(gym.Env):
 
   def close(self):
     ...
-
-
-class Connect4:
-  """
-  Simple connect4 game logic.  For two players 1 & 2.
-
-  Attributes
-    + board           {index: column}
-    + possible_moves  [column indexes]
-    + player          1 or 2
-    + winner          0 or 1 or 2 -> gameover
-                      None        -> in play
-
-  Methods
-    + show
-    + make_move( column index )
-  """
-  def __init__(self):
-    self.board = {0: [0]*6,
-                  1: [0]*6,
-                  2: [0]*6,
-                  3: [0]*6,
-                  4: [0]*6,
-                  5: [0]*6,
-                  6: [0]*6}
-    self.possible_moves = [i for i in range(7)]
-    self.player = 1
-    self.winner = None
-      
-  def make_move(self, move):
-    if move not in self.possible_moves:
-      return
-        
-    # Play their move in the top of the specified column
-    top_of_col = self.board[move].index(0)
-    self.board[move][top_of_col] = self.player
-    
-    # Players have reached the top of column so remove it from possible actions
-    if top_of_col >= 5:
-      self.possible_moves.remove(move)
-        
-    # Check if the game is over
-    if self.check_winner(move, top_of_col):
-      self.winner = self.player
-    if not self.possible_moves:
-      self.winner = 0
-
-    # Switch the player
-    self.player = 1 if self.player != 1 else 2
-          
-  def check_winner(self, col, row):
-    val = self.player
-    brd = self.board
-
-    # check right 
-    if col <= 3:
-      # Horizontal
-      if brd[col+1][row] == brd[col+2][row] == brd[col+3][row] == val:
-        return True
-      # (bounds check)  Diagonal Down 
-      if row >= 3 and brd[col+1][row-1] == brd[col+2][row-2] == brd[col+3][row-3] == val:
-        return True
-      # (bounds check)  Diagonal Up
-      if row <= 2 and brd[col+1][row+1] == brd[col+2][row+2] == brd[col+3][row+3] == val:
-        return True
-    
-    # check left
-    if col >= 3:
-      # Horizontal
-      if brd[col-1][row] == brd[col-2][row] == brd[col-3][row] == val:
-        return True
-      # (bounds check)  Diagonal Down 
-      if row >= 3 and brd[col-1][row-1] == brd[col-2][row-2] == brd[col-3][row-3] == val:
-        return True
-      # (bounds check)  Diagonal Up
-      if row <= 2 and brd[col-1][row+1] == brd[col-2][row+2] == brd[col-3][row+3] == val:
-        return True
-
-    # check vertical
-    return any([1 for key, group in groupby(brd[col]) if len(list(group)) > 3 and key == val])
-
-  def show(self):
-    print()
-    for x in range(5, -1, -1):
-      print([self.board[k][x] for k in self.board])
