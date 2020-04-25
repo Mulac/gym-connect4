26d9d1a5423315d0c0a2776ea07198ee636d0393
Mulac <wallbridge.calum@gmail.com>
2020-04-25 22:45:56 +0100

    ammended docstrings and indentation

--- gym_connect4/envs/connect4.py
+++ gym_connect4/envs/connect4.py
@@ -1,73 +1,84 @@
 import numpy as np
 from itertools import groupby
+from copy import copy
 import random
 
+
 class Connect4:
-  """
-  Simple connect4 game logic.  For two players 1 & 2.
+    """
+    Simple connect4 game logic.  For two players 1 & 2.
 
-  Attributes
-    + board           -> np.array( shape=(cols, rows) )
-    + legal_moves     -> list[int]
-    + player          -> int
-    + winner          -> int (or None)
+    Attributes
+      + board           -> np.array( shape=(cols, rows) )
+      + legal_moves     -> list[int]
+      + player          -> int
+      + winner          -> int (or None)
 
-  Methods
-    + show
-    + make_move( column index )
-  """
-  def __init__(self, cols=7, rows=6):
-    self.cols, self.rows = cols, rows
-    self.board = np.zeros(shape=(self.cols, self.rows), dtype=np.int8)
-    self.player = 1
+    Methods
+      + show
+      + make_move( column index )
+    """
 
-  @property
-  def winner(self):
-    return 1 if self.is_winner(1) \
-      else -1 if self.is_winner(-1) \
-        else 0 if self.board.all() \
-          else None
+    def __init__(self, cols=7, rows=6):
+        self.cols, self.rows = cols, rows
+        self.board = np.zeros(shape=(self.cols, self.rows), dtype=np.int8)
+        self.player = 1
 
-  @property
-  def legal_moves(self):
-    return [i for i in range(self.cols) if self.board[i][self.rows - 1] == 0]
+    @property
+    def winner(self):
+        return 1 if self.is_winner(1) \
+            else -1 if self.is_winner(-1) \
+            else 0 if self.board.all() \
+            else None
 
-  def make_move(self, move):
-    assert move in self.legal_moves
+    @property
+    def legal_moves(self):
+        # return all columns that have an empty position in the top row
+        return [i for i in range(self.cols) if self.board[i][self.rows - 1] == 0]
 
-    # Play their move in the top of the specified column
-    top_of_col = np.where(self.board[move] == 0)[0][0]
-    self.board[move][top_of_col] = self.player
+    def successor_states(self):
+        return [(action, copy(self).make_move(action).board) \
+                for action in self.legal_moves]
 
-    # Switch the player
-    self.player *= -1
-          
-  def is_winner(self, player):
-    brd = self.board
-    # Check horizontal
-    for c in range(self.cols - 3):
-      for r in range(self.rows):
-        if player == brd[c][r] == brd[c+1][r] == brd[c+2][r] == brd[c+3][r]:
-          return True
+    def clone(self):
+        return copy(self)
 
-    # Check vertical 
-    for c in range(self.cols):
-      for r in range(self.rows - 3):
-        if player == brd[c][r] == brd[c][r+1] == brd[c][r+2] == brd[c][r+3]:
-          return True
+    def make_move(self, move):
+        assert move in self.legal_moves
 
-    # Check positively sloped diaganols
-    for c in range(self.cols - 3):
-      for r in range(self.rows - 3):
-        if player == brd[c][r] == brd[c+1][r+1] == brd[c+2][r+2] == brd[c+3][r+3]:
-          return True
+        # Play their move in the top of the specified column
+        top_of_col = np.where(self.board[move] == 0)[0][0]
+        self.board[move][top_of_col] = self.player
 
-    # Check negatively sloped diaganols
-    for c in range(self.cols - 3):
-      for r in range(3, self.rows):
-        if player == brd[c][r] == brd[c+1][r-1] == brd[c+2][r-2] == brd[c+3][r-3]:
-          return True
+        # Switch the player
+        self.player *= -1
 
-  def show(self):
-    print()
-    print(np.rot90(self.board))
+    def is_winner(self, player):
+        brd = self.board
+        # Check horizontal
+        for c in range(self.cols - 3):
+            for r in range(self.rows):
+                if player == brd[c][r] == brd[c + 1][r] == brd[c + 2][r] == brd[c + 3][r]:
+                    return True
+
+        # Check vertical
+        for c in range(self.cols):
+            for r in range(self.rows - 3):
+                if player == brd[c][r] == brd[c][r + 1] == brd[c][r + 2] == brd[c][r + 3]:
+                    return True
+
+        # Check positive diagonals
+        for c in range(self.cols - 3):
+            for r in range(self.rows - 3):
+                if player == brd[c][r] == brd[c + 1][r + 1] == brd[c + 2][r + 2] == brd[c + 3][r + 3]:
+                    return True
+
+        # Check negative diagonals
+        for c in range(self.cols - 3):
+            for r in range(3, self.rows):
+                if player == brd[c][r] == brd[c + 1][r - 1] == brd[c + 2][r - 2] == brd[c + 3][r - 3]:
+                    return True
+
+    def show(self):
+        print()
+        print(np.rot90(self.board))
